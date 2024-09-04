e0bf1938ae87f1ceea8987c234e963a0c479bc32
Sebastian AR <crownedax@gmail.com>
2024-09-03 20:26:49 -0500

    ⚡️ Urls Complementos actualizadas

--- backend/Apps/Complementos/urls.py
+++ backend/Apps/Complementos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ComplementsListCreateView, ComplementsDetailView
 
 urlpatterns = [
-    path('', ComplementsListCreateView.as_view(), name='complements-list-create'),
-    path('<int:pk>/', ComplementsDetailView.as_view(), name='complements-detail'),
+    path('clients/<int:client_id>/complements/', ComplementsListCreateView.as_view(), name='complements-list-create'),
+    path('clients/<int:client_id>/complements/<int:pk>/', ComplementsDetailView.as_view(), name='complements-detail'),
 ]
