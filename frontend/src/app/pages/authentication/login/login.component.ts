55ce944495a53f8f8a1bea63fa62dfa31788d1b3
Sebastian AR <crownedax@gmail.com>
2024-10-08 13:08:49 -0500

    login/registrese eliminado

--- frontend/src/app/pages/authentication/login/login.component.ts
+++ frontend/src/app/pages/authentication/login/login.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
-import { SharedModule } from "../../../shared/shared.module";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+import { ApiService } from '../../../services/api.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
-    selector: 'app-login',
-    standalone: true,
-    imports: [SharedModule],
-    templateUrl: './login.component.html'
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  imports: [ReactiveFormsModule, CommonModule ]  // Importando módulos directamente en el componente standalone
 })
 export class LoginComponent {
-    // Tu lógica del componente
+  loginForm: FormGroup;
+  recuperarpassword = false;
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private apiService: ApiService) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      rememberMe: [false]
+    });
+  }
+
+  async onSubmit() {
+    if (this.loginForm.valid) {
+      const formValues = this.loginForm.value;
+      console.log('Formulario enviado:', formValues);
+
+      const { rememberMe, ...rest } = formValues;
+
+      try {
+        const data = await firstValueFrom(this.apiService.postData("Users/login/",rest));
+        // Guardar el token en localStorage
+        localStorage.setItem('token', data.token);
+        console.log('Login successful:', data);
+      } catch (error) {
+        console.error('Login failed:', error);
+      }
+    } else {
+      console.log('El formulario no es válido.');
+    }
+  }
+     // metodo para pasar al registro empleando funciones click en lugar de routerlink (segunda opcion)
+  goToRegister(): void {
+    // console.log("holaaaaa")
+    this.router.navigate(['/register']);
+
+  }
+
+  exito(): void {
+    this.recuperarpassword = true;
+  }
 }
--- frontend/src/app/pages/authentication/register/register.component.ts
+++ frontend/src/app/pages/authentication/register/register.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
-import { SharedModule } from "../../../shared/shared.module";
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+// Import Material
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [SharedModule],
+    imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatCheckboxModule, ReactiveFormsModule, CommonModule, RouterModule],
     templateUrl: './register.component.html'
 })
 export class RegisterComponent {
 
+  registerForm: FormGroup;
+  registroExitoso = false;
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      terms: [false, Validators.requiredTrue]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      const formValues = this.registerForm.value;
+      console.log('Formulario enviado:', formValues);
+      // Lógica para registrar al usuario.
+    } else {
+      console.log('El formulario no es válido.');
+    }
+  }
+  exito(): void {
+    this.registroExitoso = true;
+  }
 }
